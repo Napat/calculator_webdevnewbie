f8959a19c30dc2081e431ccf1f3bc9049e335ab1
Napat Rungruangbangchan <napat.joe@gmail.com>
2018-04-08 00:09:31 +0700

    add table log result, check eval() error, refactor calculate()

--- calculator/main.js
+++ calculator/main.js
@@ -1,5 +1,27 @@
+var displayRow = 1;
+
 function calculate() {
-    document.calc.output.value = eval(document.calc.output.value);
+    let result;
+    let equation = document.calc.output.value;
+
+    if(!equation) {
+        alert("Please fill the equation!!");
+        return;
+    }
+
+    try {
+        result = eval(equation);
+    }catch (err) {
+        if (err instanceof SyntaxError, TypeError) {
+            alert(err.message);
+            return;
+        }
+        throw(err);
+    }
+
+    console.log(result);
+    document.calc.output.value = result;
+    displaytableAdd(equation, result);    
 }
 
 function erase() {
@@ -8,4 +30,15 @@ function erase() {
 
 function entries(params) {
     document.calc.output.value += params;
+}
+
+function displaytableAdd(equation, result) {
+    let displaytable = document.getElementById('display');
+    let newRow = displaytable.insertRow(displayRow);
+    let cellEqu = newRow.insertCell(0);
+    let cellRes = newRow.insertCell(1);
+
+    cellEqu.innerHTML = equation;
+    cellRes.innerHTML = result;
+    displayRow++;
 }
